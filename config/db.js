284bfab07c519bfd669a796b03ed450644db5f30
jagadish-7 <coderjagadish143@gmail.com>
2025-12-08 00:40:57 +0530

    Removed hardcoded connection string

--- config/db.js
+++ config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-
-
-
 const connectDB = ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/code-flow-live");
+    mongoose.connect(process.env.MONGO_URI);
     mongoose.connection.on('connected', () => console.log('Connected with database'));
     mongoose.connection.on('error', (err) => console.log('Connection failed with - ',err));
 }
